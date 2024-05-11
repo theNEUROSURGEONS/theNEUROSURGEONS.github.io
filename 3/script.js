84d03d7e3446eb54a30e0e18b515c8949f7a7d4e
theNEUROSURGEONS <mauricewong@icloud.com>
2024-05-11 23:19:29 +0800

    r

--- 3/script.js
+++ 3/script.js
@@ -1,7 +1,8 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three'; 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
+const controls = new OrbitControls( camera, renderer.domElement ); 
 const loader = new GLTFLoader();
-
 
 
 const scene = new THREE.Scene();
