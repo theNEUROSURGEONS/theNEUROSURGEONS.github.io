085dc11a1e72e5f3e9d8af896d90f21e950aa48d
theNEUROSURGEONS <mauricewong@icloud.com>
2024-05-11 22:32:43 +0800

    e

--- 3/script.js
+++ 3/script.js
@@ -14,12 +14,15 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+var i = 1;
+
 function animate() {
 	requestAnimationFrame( animate );
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
+    console.log (i);
+    i = i + 1;
 	renderer.render( scene, camera );
 }
 
